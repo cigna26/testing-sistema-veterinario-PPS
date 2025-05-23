6ea2836ea654ca9968327bd25f721529687df96a
cigna <whitebownitro@gmail.com>
2025-05-23 15:40:33 -0400

    Se añade test para buscar mascotas existentes

--- test_sistema_veterinario.py
+++ test_sistema_veterinario.py
@@ -8,7 +8,19 @@ def limpiar_mascotas():
     mascotas.clear()
     return mascotas
 
+# TEST UNITARIOS
+
 def test_registro(limpiar_mascotas):
     registrar_mascota('Luna', 'gato', '3', 'Ana')
     assert len(limpiar_mascotas) == 1
     assert limpiar_mascotas[0]['nombre'] == 'Luna'
+
+
+def test_busqueda_existente(limpiar_mascotas):
+    limpiar_mascotas.append({'nombre': 'Max',
+                            'especie': 'perro',
+                            'edad': '2',
+                            'duenio': 'Luis'})
+    resultado = any(m['nombre'].lower() == 'max' for m in limpiar_mascotas)
+    assert resultado is True
+
