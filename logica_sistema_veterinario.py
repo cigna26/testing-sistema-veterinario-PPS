970939cbbfacb10bd0cd51423a204466689b8695
cigna <whitebownitro@gmail.com>
2025-05-23 10:22:14 -0400

    Se añade logica d registrar

--- logica_sistema_veterinario.py
+++ logica_sistema_veterinario.py
@@ -1,3 +1,14 @@
 # Logica del sistema veterinario para facilitar el uso de testing
 
 
+mascotas = []
+
+def registrar_mascota(nombre, especie, edad, duenio):
+    mascota = {
+        "nombre": nombre,
+        "especie": especie,
+        "edad": edad,
+        "duenio": duenio
+    }
+    mascotas.append(mascota)
+
